91cf854a21cccd7d9a12c372d667cf7acf9e189f
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-31 23:27:15 +0800

    Gate the app behind account or guest access

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AccessGate } from "./access";
 import "./styles.css";
 import "./ui-consistency.css";
+import "./access.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AccessGate>
+      <App />
+    </AccessGate>
   </React.StrictMode>,
 );
 
