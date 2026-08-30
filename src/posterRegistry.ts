a07137f25fd861aec5259acf474b76e80da28bf6
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-08-30 11:56:22 +0800

    Add verified Damai poster fallbacks

--- src/posterRegistry.ts
+++ src/posterRegistry.ts
@@ -12,3 +12,20 @@ export function normalizeLegacyAssetUrl(src?: string) {
     return "";
   }
 }
+
+
+const DAMAI_POSTER_FALLBACKS: Record<string, string> = {
+  "1060966698015": "https://img1.tking.cn/mtl/default/img/2026/7/10/pRGMtzBCAN_.jpg?x-oss-process=image%2Fresize%2Cw_1200%2Fquality%2Cq_90",
+  "1064771079264": "https://img1.tking.cn/mtl/default/img/2026/7/15/Yxe44itf5e_.jpg?x-oss-process=image%2Fresize%2Cw_1200%2Fquality%2Cq_90",
+};
+
+const DAMAI_SEAT_MAP_FALLBACKS: Record<string, string> = {
+  "1064771079264": "https://inews.gtimg.com/om_bt/Oh4MIEz8I0RJLjTyQ8NobPmkL36jp_ZZGjBLyRMdNdhG0AA/641",
+};
+
+export function knownDamaiMedia(itemId: string) {
+  return {
+    posterUrl: normalizeLegacyAssetUrl(DAMAI_POSTER_FALLBACKS[itemId]),
+    seatMapUrl: normalizeLegacyAssetUrl(DAMAI_SEAT_MAP_FALLBACKS[itemId]),
+  };
+}
